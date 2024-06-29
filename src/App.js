1200fd9b1bcc75b8e04a4c3276c56e93f66289dc
Aurelie04 <aurelienana25@gmail.com>
2024-06-29 21:19:37 +0200

    building and finishup Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Home />} />  {/* Use element prop for JSX syntax */}
+          <Route path="/" exact element={Home } />  
         </Routes>
       </Router>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,18 @@ import { Button } from '@mui/material';
 import React from 'react';
 import {Link} from 'react-router-dom';
 import BackgrongImage from "../assets/pizzaBckground.jpeg";
+import"../styles/Home.css";
 
 function Home() {
   return 
-  <div className='home'>
-      <div className='headerContainer' style={{ background: `url({BackgrongImage})` }}>
+  <div className='home' style={{ background: `url({BackgrongImage})` }}>
+      <div className='headerContainer'>
         <h1>Delicious Pizzeria </h1>
         <p> PIZZA TO FIT ANY TASTE</p>
         <Link to="menu">
           <button> ORDER NOW </button>
         </Link>
+        
         
       </div>
   </div>
