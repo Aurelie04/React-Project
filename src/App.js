24ac5083dc4e5acdae1fd4eede9f91ff62f201f8
Aurelie04 <aurelienana25@gmail.com>
2024-06-29 19:52:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Route,Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 
 function App() {
@@ -9,12 +9,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-           <Route path="/" exact Component={<Home />} />
+          <Route path="/" exact element={<Home />} />  {/* Use element prop for JSX syntax */}
         </Routes>
       </Router>
-      
     </div>
   );
 }
 
 export default App;
+  
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Button } from '@mui/material';
+import React from 'react';
+import {Link} from 'react-router-dom';
+import BackgrongImage from "../assets/pizzaBckground.jpeg";
 
 function Home() {
-  return (
-    <div>Hello</div>
-  )
+  return 
+  <div className='home'>
+      <div className='headerContainer' style={{ background: `url({BackgrongImage})` }}>
+        <h1>Delicious Pizzeria </h1>
+        <p> PIZZA TO FIT ANY TASTE</p>
+        <Link to="menu">
+          <button> ORDER NOW </button>
+        </Link>
+        
+      </div>
+  </div>
+  
 }
 
 export default Home
